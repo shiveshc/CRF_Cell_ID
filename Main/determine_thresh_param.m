7d3821453286090783bf7b3136706bde0e1f9247
Chaudhary, Shivesh <schaudhary9@gatech.edu>
2020-03-19 18:19:30 -0400

    updating step-by-step walkthrough

--- Main/determine_thresh_param.m
+++ Main/determine_thresh_param.m
@@ -15,10 +15,12 @@ function [thresh_param,accept_thresh,index_thresh_new] = determine_thresh_param(
     z_thresh = z(peaks>thresh_val); 
     index_thresh = index(peaks>thresh_val);
     [x_thresh_new,y_thresh_new,z_thresh_new,index_thresh_new] = remove_close_neurons(x_thresh,y_thresh,z_thresh,index_thresh,curr_img);
-    figure,imshow(max(curr_img,[],3),[])
+    figure,imshow(max(curr_img,[],3),[],'border','tight')
     caxis([0,0.6])
     hold on
     scatter(y_thresh,x_thresh,'.g')
     scatter(y_thresh_new,x_thresh_new,'.r')
+    text(30,30,['Number of detected cells - ',num2str(size(x_thresh_new,1))],'Color','white')
     accept_thresh = input("Enter 'y' if accept thresh_parameter else enter 'n' -");
+    close all
 end
